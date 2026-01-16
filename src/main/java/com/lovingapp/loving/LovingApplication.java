6ac4b075022674baebfd42b222234201c18d671e
parulpragya767 <parulpragya767@gmail.com>
2026-01-16 20:18:09 +0530

    cleaning up config scan

--- src/main/java/com/lovingapp/loving/LovingApplication.java
+++ src/main/java/com/lovingapp/loving/LovingApplication.java
@@ -2,25 +2,13 @@ package com.lovingapp.loving;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.lovingapp.loving.config.database.SupabaseProperties;
-import com.lovingapp.loving.config.llm.LlmClientProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@EnableConfigurationProperties({ LlmClientProperties.class, SupabaseProperties.class })
-@RestController
+@ConfigurationPropertiesScan
 public class LovingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LovingApplication.class, args);
-    }
-
-    @GetMapping("/sayHello")
-    public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
     }
 }
