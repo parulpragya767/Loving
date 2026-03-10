319a0c60fa288bbddc106806f898767a975d2786
parulpragya767 <parulpragya767@gmail.com>
2026-03-10 10:25:36 +0530

    feature flag config and service

---
+++ src/main/java/com/lovingapp/config/app/FeatureFlagsProperties.java
@@ -0,0 +1,12 @@
+package com.lovingapp.config.app;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@Data
+@ConfigurationProperties(prefix = "app.features")
+public class FeatureFlagsProperties {
+
+    private boolean paymentsEnabled;
+}
---
+++ src/main/java/com/lovingapp/service/FeatureFlagService.java
@@ -0,0 +1,18 @@
+package com.lovingapp.service;
+
+import org.springframework.stereotype.Service;
+
+import com.lovingapp.config.app.FeatureFlagsProperties;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class FeatureFlagService {
+
+    private final FeatureFlagsProperties featureFlags;
+
+    public boolean isPaymentsEnabled() {
+        return featureFlags.isPaymentsEnabled();
+    }
+}
