55ec08d6250d9b75389c5e3cacb921c7df84bc12
parulpragya767 <parulpragya767@gmail.com>
2026-04-10 11:40:03 +0530

    reading env property files based on spring profile

--- src/main/java/com/lovingapp/LovingApplication.java
+++ src/main/java/com/lovingapp/LovingApplication.java
@@ -1,14 +1,30 @@
 package com.lovingapp;
 
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class LovingApplication {
 
     public static void main(String[] args) {
+        String profile = Optional.ofNullable(System.getenv("SPRING_PROFILES_ACTIVE"))
+                .orElse("dev");
+
+        String filename = ".env" + (profile.equals("dev") ? "" : "." + profile);
+
+        Dotenv dotenv = Dotenv.configure()
+                .filename(filename)
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(LovingApplication.class, args);
     }
 }
